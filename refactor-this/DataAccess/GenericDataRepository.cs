934122a52f8cca626e6c4cf31a52e1165b255c73
Alina Goddard <alina.longstaff@gmail.com>
2018-12-07 08:18:26 +1300

    add database

---
+++ refactor-this/DataAccess/GenericDataRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess
+{
+    public class GenericDataRepository<T> : IGenericDataRepository<T> where T : class
+    {
+        public virtual IList<T> GetAll(string connectionString, Guid id)
+        {
+
+        }
+
+        public virtual IList<T> GetEntities(string connectionString, Guid id)
+        {
+            using (var context = new AccountsDatabase(connectionString))
+            {
+                //TODO: how to do this
+                return context.Transactions.AsNoTracking().Where(t => t.AccountId == id).ToList();
+            }
+        }
+
+        public virtual bool Add(string connectionString, T itemToAdd, Guid id)
+        {
+
+        }
+    }
+}
+
---
+++ refactor-this/DataAccess/IGenericDataRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess
+{
+    public interface IGenericDataRepository<T> where T : class
+    {
+        IList<T> GetAll(string connectionString, Guid id);
+        IList<T> GetEntities(string connectionString, Guid id);
+        bool Add(string connectionString, T itemToAdd, Guid id);
+    }
+}
+
